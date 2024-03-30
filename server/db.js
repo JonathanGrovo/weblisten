c083fb3e7b62f7bd10153da79af9c4ac819a5ef0
Jonathan Grovo <jonathan.grovo@gmail.com>
2024-03-29 21:00:49 -0400

    db.js change

--- server/db.js
+++ server/db.js
@@ -1,13 +1,11 @@
 const mongoose = require('mongoose');
 
 const uri = "mongodb+srv://jonathangrovo:QD5q5IhtZ7PQAhXZ@weblisten.1kqhjr9.mongodb.net/?retryWrites=true&w=majority&appName=weblisten";
-// const client = new MongoClient(uri);
 
 const connectToMongoDB = async () => {
     try {
         await mongoose.connect(uri);
         console.log('Connected to MongoDB');
-        // return client.db('weblisten'); // Return the database instance
     } catch (err) {
         console.error("Failed to connect to MongoDB", err);
         throw err;
